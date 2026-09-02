Lokesh7025/Kepler

feat(sandbox): harden local isolation and add unsafe mode
Signed-off-by: Lokesh <lokeshselvam7025@gmail.com>

packages/sandbox/src/platform.ts
@@ -1,7 +1,12 @@
 // SPDX-FileCopyrightText: 2026 Hari Srinivasan
 // SPDX-License-Identifier: Apache-2.0
 
-import type { KernelTool, ShellToolOptions, WorkspacePolicy } from "@kepler/kernel";
+import {
+  makeShellTool,
+  type KernelTool,
+  type ShellToolOptions,
+  type WorkspacePolicy,
+} from "@kepler/kernel";
 import type { EventPayloadMap } from "@kepler/protocol";
 
 import {
@@ -18,6 +23,7 @@ import {
   makeSeatbeltShellTool,
   seatbeltConfiguredPayload,
 } from "./seatbelt.ts";
+import { definedEnvironment } from "./environment.ts";
 
 export interface PlatformShellOptions extends Omit<ShellToolOptions, "wrapCommand"> {
   readonly policy: WorkspacePolicy;
@@ -38,6 +44,23 @@ export interface PlatformSandbox {
     readonly env?: Readonly<Record<string, string | undefined>>;
   }): SandboxedProcess;
   configuredPayload(): EventPayloadMap["sandbox.configured"];
+}
+
+/** Explicit unconfined execution for the `--unsafe` startup mode. */
+export function createUnsafePlatformExecution(): PlatformSandbox {
+  return {
+    provider: "none",
+    available: true,
+    reason: "operating-system isolation disabled by --unsafe",
+    makeShellTool: ({ policy: _policy, ...options }) => makeShellTool(options),
+    wrapProcess: ({ command, args, cwd, env = process.env }) => ({
+      command,
+      args,
+      cwd,
+      env: definedEnvironment(env),
+    }),
+    configuredPayload: () => ({ provider: "none", enforced: false, controls: [] }),
+  };
 }
 
 /**

packages/tui/test/app.test.ts
@@ -10,7 +10,7 @@ import test, { type TestContext } from "node:test";
 
 import { KeplerDaemon, DaemonClient, type SessionInteractionRequest } from "@kepler/daemon";
 import { type ModelPort, ToolRegistry } from "@kepler/kernel";
-import type { JsonObject, ModelStreamEvent, Usage } from "@kepler/protocol";
+import type { EventPayloadMap, JsonObject, ModelStreamEvent, Usage } from "@kepler/protocol";
 
 import { KeplerApp } from "../src/index.ts";
 
@@ -37,6 +37,7 @@ async function startStack(
       content?: JsonObject;
     }>,
   ) => ToolRegistry = () => new ToolRegistry(),
+  sandbox?: EventPayloadMap["sandbox.configured"],
 ) {
   const directory = await mkdtemp(join(tmpdir(), "kepler-tui-"));
   context.after(() => rm(directory, { recursive: true, force: true }));
@@ -48,6 +49,7 @@ async function startStack(
       model,
       tools: makeTools(interact),
       system: "You are Kepler.",
+      ...(sandbox === undefined ? {} : { sandbox }),
       ...(selection.modelId === undefined ? {} : { configModel: { modelId: selection.modelId } }),
       ...(selection.thinkingLevel === undefined
         ? {}
@@ -136,6 +138,33 @@ test("a full round trip: type, send, render the reply, detach, resume", async (c
   assert.match(resumed.text(), /│ hello kepler/);
   assert.match(resumed.text(), /the answer/);
   resumedApp.stop();
+});
+
+test("an unenforced session keeps a persistent unsafe warning", async (context) => {
+  const { socketPath, directory } = await startStack(context, port, undefined, {
+    provider: "none",
+    enforced: false,
+    controls: [],
+  });
+  const input = new PassThrough();
+  const { output, text } = captureOutput();
+  const app = await KeplerApp.start({
+    client: await DaemonClient.connect(socketPath),
+    input,
+    output,
+    cwd: directory,
+    color: false,
+  });
+  const warning = "UNSAFE: no sandbox; tools have full host access";
+  assert.match(text(), new RegExp(warning));
+  input.write("/theme\r");
+  await until(() => text().includes("Select theme"), "unsafe theme dialog");
+  assert.match(text(), new RegExp(warning));
+  input.write("\x1b");
+  input.write("hello\r");
+  await until(() => text().includes("the answer"), "unsafe assistant reply");
+  assert.match(text(), new RegExp(warning));
+  app.stop();
 });
 
 test("editing, /quit, and busy notices behave", async (context) => {

Haz3-jolt/Axl

packages/sandbox/src/platform.ts
@@ -1,7 +1,12 @@
 // SPDX-FileCopyrightText: 2026 Hari Srinivasan
 // SPDX-License-Identifier: Apache-2.0
 
-import type { KernelTool, ShellToolOptions, WorkspacePolicy } from "@axl/kernel";
+import {
+  makeShellTool,
+  type KernelTool,
+  type ShellToolOptions,
+  type WorkspacePolicy,
+} from "@axl/kernel";
 import type { EventPayloadMap } from "@axl/protocol";
 
 import {
@@ -18,6 +23,7 @@ import {
   makeSeatbeltShellTool,
   seatbeltConfiguredPayload,
 } from "./seatbelt.ts";
+import { definedEnvironment } from "./environment.ts";
 
 export interface PlatformShellOptions extends Omit<ShellToolOptions, "wrapCommand"> {
   readonly policy: WorkspacePolicy;
@@ -38,6 +44,23 @@ export interface PlatformSandbox {
     readonly env?: Readonly<Record<string, string | undefined>>;
   }): SandboxedProcess;
   configuredPayload(): EventPayloadMap["sandbox.configured"];
+}
+
+/** Explicit unconfined execution for the `--unsafe` startup mode. */
+export function createUnsafePlatformExecution(): PlatformSandbox {
+  return {
+    provider: "none",
+    available: true,
+    reason: "operating-system isolation disabled by --unsafe",
+    makeShellTool: ({ policy: _policy, ...options }) => makeShellTool(options),
+    wrapProcess: ({ command, args, cwd, env = process.env }) => ({
+      command,
+      args,
+      cwd,
+      env: definedEnvironment(env),
+    }),
+    configuredPayload: () => ({ provider: "none", enforced: false, controls: [] }),
+  };
 }
 
 /**

packages/tui/test/app.test.ts
@@ -11,7 +11,7 @@ import test, { type TestContext } from "node:test";
 import { AZURE_OPENAI_MODELS } from "@axl/ai";
 import { DaemonClient, AxlDaemon, type SessionInteractionRequest } from "@axl/daemon";
 import { type ModelPort, ToolRegistry } from "@axl/kernel";
-import type { JsonObject, ModelStreamEvent, Usage } from "@axl/protocol";
+import type { EventPayloadMap, JsonObject, ModelStreamEvent, Usage } from "@axl/protocol";
 
 import { AxlApp } from "../src/index.ts";
 
@@ -38,6 +38,7 @@ async function startStack(
       content?: JsonObject;
     }>,
   ) => ToolRegistry = () => new ToolRegistry(),
+  sandbox?: EventPayloadMap["sandbox.configured"],
 ) {
   const directory = await mkdtemp(join(tmpdir(), "axl-tui-"));
   context.after(() => rm(directory, { recursive: true, force: true }));
@@ -49,6 +50,7 @@ async function startStack(
       model,
       tools: makeTools(interact),
       system: "You are Axl.",
+      ...(sandbox === undefined ? {} : { sandbox }),
       ...(selection.modelId === undefined ? {} : { configModel: { modelId: selection.modelId } }),
       ...(selection.thinkingLevel === undefined
         ? {}
@@ -141,6 +143,33 @@ test("a full round trip: type, send, render the reply, detach, resume", async (c
   assert.match(resumed.text(), /│ hello axl/);
   assert.match(resumed.text(), /the answer/);
   resumedApp.stop();
+});
+
+test("an unenforced session keeps a persistent unsafe warning", async (context) => {
+  const { socketPath, directory } = await startStack(context, port, undefined, {
+    provider: "none",
+    enforced: false,
+    controls: [],
+  });
+  const input = new PassThrough();
+  const { output, text } = captureOutput();
+  const app = await AxlApp.start({
+    client: await DaemonClient.connect(socketPath),
+    input,
+    output,
+    cwd: directory,
+    color: false,
+  });
+  const warning = "UNSAFE: no sandbox; tools have full host access";
+  assert.match(text(), new RegExp(warning));
+  input.write("/theme\r");
+  await until(() => text().includes("Select theme"), "unsafe theme dialog");
+  assert.match(text(), new RegExp(warning));
+  input.write("\x1b");
+  input.write("hello\r");
+  await until(() => text().includes("the answer"), "unsafe assistant reply");
+  assert.match(text(), new RegExp(warning));
+  app.stop();
 });
 
 test("fork, clone, and resume switch sessions through the daemon", async (context) => {